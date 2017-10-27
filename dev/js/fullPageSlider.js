601a08040bb6e433ac85fc67ad7259000696810a
Oberyn18 <carlosab1802@gmail.com>
2017-10-26 22:10:38 -0200

    Update 'getSections' and 'getActualSectionInd'.

--- dev/js/fullPageSlider.js
+++ dev/js/fullPageSlider.js
@@ -11,38 +11,54 @@ const changeScroll = (newPos) => {
     document.scrollingElement.scrollTop = newPos;
 };
 
-// Retorna la sección sobre la cual se está actualmente
-const getActualSection = (sections) => {
+// Retorna el índice de la sección sobre la cual se está actualmente
+const getActualSectionInd = (sections) => {
     let actualScroll = getCurrentPosition();
+    // Itero hasta el penúltimo, ya que lo que quiero son las diferencias entre los consecutivos (y llegaré al último de todas formas)
     for (let i = 0; i < sections.length - 1; i++) {
-        let difference = Math.floor((sections[i] + sections[i+1]) / 2) ;
-        if (actualScroll <  difference) {
-            return [sections[i], i];
+        // El promedio de las posiciones de las seccion consecutivas es la posición de la mitad entre dichas secciones
+        let medianPos = Math.floor((sections[i][1] + sections[i+1][1]) / 2) ;
+        if (actualScroll <  medianPos) {
+            // Si no paso esa mitad, estoy donde debo estar, pues las posiciones las estoy recorriendo en orden ascendente.
+            return i;
         }
     }
-    return [sections[sections.length-1], sections.length-1];
+    // Si nunca estuve antes de la diferencia entre las consecutivas, debo estar al final de todo.
+    return sections.length - 1;
 };
 
-// Retorna todas las posiciones de las secciones escogidas, en un array
-const getSections = () => {
-    let e1 = document.getElementById('header');
-    let e2 = document.getElementById('about');
-    let e3 = document.getElementById('skills');
-    let e4 = document.getElementById('works');
-    let e5 = document.getElementById('contact');
-    let e6 = document.getElementById('footer');
-    return [getElementPosition(e1),
-        getElementPosition(e2),
-        getElementPosition(e3),
-        getElementPosition(e4),
-        getElementPosition(e5),
-        getElementPosition(e6)];
+// Recibe el nombre de los Id's de las secciones y retorna todas las secciones y sus posiciones, en un array bidimensional
+const getSections = (sectionsIds) => {
+    let sections = new Array();
+    for (let sectionId of sectionsIds) {
+        let e = document.getElementById(sectionId);
+        let position = getElementPosition(e);
+        sections.push([e, position]);
+    }
+    return sections;
 };
 
 // Función que simula el "easing"
 const easing = function(progress) {
     return progress < 0.5 ? 4 * progress * progress * progress : (progress - 1) * (2 * progress - 2) * (2 * progress - 2) + 1;    
 }
+
+const fullPageSlider = () => {
+    // Esta variable (sectionsNames) se debe sobreescribir siempre que se use este slider en distintos proyectos.
+    let sectionsNames = ['header', 'about', 'skills', 'works', 'contact', 'footer'];
+    // Contiene pares (elementoHTML, posición en Y) sobre los cuales haremos el scroll
+    let sections = getSections(sectionsNames);
+    // Ahora, se debe encontrar la posición inicial cuando carga el slider.
+    let actualPosInd = getActualSectionInd(section);
+    // Ahora hay que estabilizar la sección
+    
+
+};
+fullPageSlider();
+
+const initWheelMovement = () => {
+    wheelMovement(sections, actualSection);
+};
 
 // Evento para la rueda del ratón
 const wheelMovement = (sections, actualSection) => {
@@ -127,14 +143,92 @@ const stabilizeSection = (sections) => {
     return actualSectionInd;
 };
 
-const initWheelMovement = () => {
+
+const moveToSection = (direction, sections) => {
+    let startPos = getActualSection(sections)[0];
+    let actualSectionInd = getActualSection(sections)[1];
+    let total = sections.length;
+    if (direction == 'down') {
+        ++actualSectionInd;
+    } else if (direction == 'up'){
+        if (actualSectionInd != 0) {
+            --actualSectionInd;
+        }else {
+            actualSectionInd = total - 1;
+        }
+    } else {
+        console.log('ERROR!');
+    }
+    let finalPos = sections[actualSectionInd + 1];
+    console.log("Posición actual: ", startPos);
+    console.log("Sección actual: ", actualSectionInd);
+    let runAnimation; // guardará el animation frame
+    let timeLapsed = 0; // acumulador del tiempo transcurrido
+    let percentage, position; // contenedores de las posiciones actuales y porcentaje avanzado
+    let duration = 2000; // duración deseada
+    let distance = finalPos - startPos; // distancia entre ambas posiciones
+    const animate = () => {
+        timeLapsed += 16;
+        percentage = timeLapsed / duration;
+        percentage = percentage > 1 ? 1 : percentage;
+        position = startPos + distance * easing(percentage);
+        changeScroll(position);
+        runAnimation = requestAnimationFrame(animate);
+        if (position == finalPos) {
+            cancelAnimationFrame(runAnimation);
+            setTimeout(function () {
+                
+            }, 2000);  
+        }
+    };
+    runAnimation = requestAnimationFrame(animate);
+}
+
+
+
+
+// ---------------------------------------------------------- TOUCHSCREEN !!!! -------------------------------------------------
+const swipeDetect = (sections) => {
+    let swipedir,
+    startX,
+    startY,
+    distX,
+    distY,
+    threshold = 150, //required min distance traveled to be considered swipe
+    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+    allowedTime = 1000, // maximum time allowed to travel that distance
+    elapsedTime,
+    startTime;
+    window.addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        let touchobj = e.changedTouches[0]
+        swipedir = 'none';
+        startX = touchobj.pageX;
+        startY = touchobj.pageY;
+        startTime = new Date().getTime() // record time when finger first makes contact with surface
+    }, false);
+    
+    window.addEventListener("touchend", (e) => {
+        e.preventDefault()
+        let touchobj = e.changedTouches[0];
+        distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        if (elapsedTime <= allowedTime){ // first condition for awipe met
+            if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+                swipedir = (distY < 0)? 'down' : 'up' // if dist traveled is negative, it indicates up swipe
+            }
+        }
+        console.log(swipedir);
+        moveToSection(swipedir, sections);
+    });
+
+}
+const initSwipeMovement = () => {
     let sections = getSections();
-    let actualSection = stabilizeSection(sections);
-    wheelMovement(sections, actualSection);
+    swipeDetect(sections);
 };
-
-initWheelMovement();
-
+// initSwipeMovement();
 
 // wheelMovement(getSections());
 
--- docs/js/fullPageSlider.js
+++ docs/js/fullPageSlider.js
@@ -15,32 +15,74 @@ var changeScroll = function changeScroll(newPos) {
     document.scrollingElement.scrollTop = newPos;
 };
 
-// Retorna la sección sobre la cual se está actualmente
-var getActualSection = function getActualSection(sections) {
+// Retorna el índice de la sección sobre la cual se está actualmente
+var getActualSectionInd = function getActualSectionInd(sections) {
     var actualScroll = getCurrentPosition();
+    // Itero hasta el penúltimo, ya que lo que quiero son las diferencias entre los consecutivos (y llegaré al último de todas formas)
     for (var i = 0; i < sections.length - 1; i++) {
-        var difference = Math.floor((sections[i] + sections[i + 1]) / 2);
-        if (actualScroll < difference) {
-            return [sections[i], i];
+        // El promedio de las posiciones de las seccion consecutivas es la posición de la mitad entre dichas secciones
+        var medianPos = Math.floor((sections[i][1] + sections[i + 1][1]) / 2);
+        if (actualScroll < medianPos) {
+            // Si no paso esa mitad, estoy donde debo estar, pues las posiciones las estoy recorriendo en orden ascendente.
+            return i;
         }
     }
-    return [sections[sections.length - 1], sections.length - 1];
+    // Si nunca estuve antes de la diferencia entre las consecutivas, debo estar al final de todo.
+    return sections.length - 1;
 };
 
-// Retorna todas las posiciones de las secciones escogidas, en un array
-var getSections = function getSections() {
-    var e1 = document.getElementById('header');
-    var e2 = document.getElementById('about');
-    var e3 = document.getElementById('skills');
-    var e4 = document.getElementById('works');
-    var e5 = document.getElementById('contact');
-    var e6 = document.getElementById('footer');
-    return [getElementPosition(e1), getElementPosition(e2), getElementPosition(e3), getElementPosition(e4), getElementPosition(e5), getElementPosition(e6)];
+// Recibe el nombre de los Id's de las secciones y retorna todas las secciones y sus posiciones, en un array bidimensional
+var getSections = function getSections(sectionsIds) {
+    var sections = new Array();
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = sectionsIds[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var sectionId = _step.value;
+
+            var e = document.getElementById(sectionId);
+            var position = getElementPosition(e);
+            sections.push([e, position]);
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+
+    return sections;
 };
 
 // Función que simula el "easing"
 var easing = function easing(progress) {
     return progress < 0.5 ? 4 * progress * progress * progress : (progress - 1) * (2 * progress - 2) * (2 * progress - 2) + 1;
+};
+
+var fullPageSlider = function fullPageSlider() {
+    // Esta variable (sectionsNames) se debe sobreescribir siempre que se use este slider en distintos proyectos.
+    var sectionsNames = ['header', 'about', 'skills', 'works', 'contact', 'footer'];
+    // Contiene pares (elementoHTML, posición en Y) sobre los cuales haremos el scroll
+    var sections = getSections(sectionsNames);
+    // Ahora, se debe encontrar la posición inicial cuando carga el slider.
+    var actualPosInd = getActualSectionInd(section);
+    // Ahora hay que estabilizar la sección
+
+};
+fullPageSlider();
+
+var initWheelMovement = function initWheelMovement() {
+    wheelMovement(sections, actualSection);
 };
 
 // Evento para la rueda del ratón
@@ -126,13 +168,91 @@ var stabilizeSection = function stabilizeSection(sections) {
     return actualSectionInd;
 };
 
-var initWheelMovement = function initWheelMovement() {
-    var sections = getSections();
-    var actualSection = stabilizeSection(sections);
-    wheelMovement(sections, actualSection);
+var moveToSection = function moveToSection(direction, sections) {
+    var startPos = getActualSection(sections)[0];
+    var actualSectionInd = getActualSection(sections)[1];
+    var total = sections.length;
+    if (direction == 'down') {
+        ++actualSectionInd;
+    } else if (direction == 'up') {
+        if (actualSectionInd != 0) {
+            --actualSectionInd;
+        } else {
+            actualSectionInd = total - 1;
+        }
+    } else {
+        console.log('ERROR!');
+    }
+    var finalPos = sections[actualSectionInd + 1];
+    console.log("Posición actual: ", startPos);
+    console.log("Sección actual: ", actualSectionInd);
+    var runAnimation = void 0; // guardará el animation frame
+    var timeLapsed = 0; // acumulador del tiempo transcurrido
+    var percentage = void 0,
+        position = void 0; // contenedores de las posiciones actuales y porcentaje avanzado
+    var duration = 2000; // duración deseada
+    var distance = finalPos - startPos; // distancia entre ambas posiciones
+    var animate = function animate() {
+        timeLapsed += 16;
+        percentage = timeLapsed / duration;
+        percentage = percentage > 1 ? 1 : percentage;
+        position = startPos + distance * easing(percentage);
+        changeScroll(position);
+        runAnimation = requestAnimationFrame(animate);
+        if (position == finalPos) {
+            cancelAnimationFrame(runAnimation);
+            setTimeout(function () {}, 2000);
+        }
+    };
+    runAnimation = requestAnimationFrame(animate);
 };
 
-initWheelMovement();
+// ---------------------------------------------------------- TOUCHSCREEN !!!! -------------------------------------------------
+var swipeDetect = function swipeDetect(sections) {
+    var swipedir = void 0,
+        startX = void 0,
+        startY = void 0,
+        distX = void 0,
+        distY = void 0,
+        threshold = 150,
+        //required min distance traveled to be considered swipe
+    restraint = 100,
+        // maximum distance allowed at the same time in perpendicular direction
+    allowedTime = 1000,
+        // maximum time allowed to travel that distance
+    elapsedTime = void 0,
+        startTime = void 0;
+    window.addEventListener("touchstart", function (e) {
+        e.preventDefault();
+        var touchobj = e.changedTouches[0];
+        swipedir = 'none';
+        startX = touchobj.pageX;
+        startY = touchobj.pageY;
+        startTime = new Date().getTime(); // record time when finger first makes contact with surface
+    }, false);
+
+    window.addEventListener("touchend", function (e) {
+        e.preventDefault();
+        var touchobj = e.changedTouches[0];
+        distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime; // get time elapsed
+        if (elapsedTime <= allowedTime) {
+            // first condition for awipe met
+            if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) {
+                // 2nd condition for vertical swipe met
+                swipedir = distY < 0 ? 'down' : 'up'; // if dist traveled is negative, it indicates up swipe
+            }
+        }
+        console.log(swipedir);
+        moveToSection(swipedir, sections);
+    });
+};
+var initSwipeMovement = function initSwipeMovement() {
+    var sections = getSections();
+    swipeDetect(sections);
+};
+// initSwipeMovement();
 
 // wheelMovement(getSections());
 
